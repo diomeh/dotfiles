8dff1c0ffc9f38ac7285614de685905a497a7669
David Urbina <davidurbina.dev@gmail.com>
2025-11-23 01:05:26 -0500

    Updates monospace font configuration
    Adds "Symbols Nerd Font" and "DejaVu Sans Mono" to the default monospace font list.
    
    This ensures better symbol support in terminal applications and provides a fallback option if GeistMono is unavailable.

--- modules/nixos/settings/fonts.nix
+++ modules/nixos/settings/fonts.nix
@@ -4,13 +4,17 @@
   fonts.fontconfig.enable = true;
   fonts.fontDir.enable = true;
 
-  fonts.fontconfig.defaultFonts.monospace = [ "GeistMono" ];
-
   # Nerd Fonts
   fonts.packages = with pkgs; [
     corefonts
     vistafonts
     nerd-fonts.symbols-only
     nerd-fonts.geist-mono
+  ];
+
+  fonts.fontconfig.defaultFonts.monospace = [ 
+    "GeistMono"
+    "Symbols Nerd Font"
+    "DejaVu Sans Mono"
   ];
 }

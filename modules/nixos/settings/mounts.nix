999220c1336869807d6f71f25b03ea356923bad3
David Urbina <davidurbina.dev@gmail.com>
2025-11-23 00:38:16 -0500

    Adds ntfs partition mount
    Adds a new ntfs partition mount for the windows drive.
    
    This allows the system to automatically mount the drive on boot, and
    grants user access through uid mapping.

--- modules/nixos/settings/mounts.nix
+++ modules/nixos/settings/mounts.nix
@@ -1,11 +1,5 @@
 { pkgs, ... }:
 {
-  # ext4 partition
-  fileSystems."/mnt/drive" =
-    { device = "/dev/disk/by-uuid/e47201c1-b95a-442b-a1e1-36caeba0babc";
-      fsType = "ext4";
-    };
-
   # ntfs partition
   environment.systemPackages = [
     pkgs.ntfs3g
@@ -13,6 +7,12 @@
 
   fileSystems."/mnt/ntfs" =
     { device = "/dev/disk/by-uuid/D6744E28744E0C25";
+      fsType = "ntfs-3g"; 
+      options = [ "rw" "uid=1000"];
+    };
+
+  fileSystems."/mnt/windows" =
+    { device = "/dev/disk/by-uuid/30AC5CF1AC5CB358";
       fsType = "ntfs-3g"; 
       options = [ "rw" "uid=1000"];
     };

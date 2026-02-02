9a17191ecdd16dd125ffa861a2290e9c831c2b40
David Urbina <davidurbina.dev@gmail.com>
2026-02-02 17:50:15 -0500

    update godot to 4.6

--- modules/nixos/packages/gamedev.nix
+++ modules/nixos/packages/gamedev.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    godot # Game engine
+    godotPackages_4_6.godot # Game engine
     blender # 3D modeling and animation suite
     obs-studio # Free and open source software for video recording and live streaming
     aseprite # Pixel art tool (requires license)
